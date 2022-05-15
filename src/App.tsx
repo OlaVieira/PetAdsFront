83104a1854a6186eb499dd60365f68f1fc68e1a3
Aleksandra Zwierzchowska <a.zwierzchowska32@gmail.com>
2022-05-15 20:06:55 +0200

    Basic structure of main view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+            <strong>Petsitters</strong> ads <i className="fa-solid fa-paw"></i>
+        </h1>
+
+          <button className="main-button">Add an ad</button>
+          <div className="search">
+              <input type="text"/> <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
+          </div>
       </header>
-    </div>
+      <div className="map">
+
+      </div>
+    </>
   );
 }
 
-export default App;
+
